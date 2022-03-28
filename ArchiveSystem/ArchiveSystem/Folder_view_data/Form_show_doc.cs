55197fc268ef824bf172d7d890af55f40c6ea2e9
mohammed01it <91244043+mohammed01it@users.noreply.github.com>
2022-03-28 11:20:31 +0300

    com2

---
+++ ArchiveSystem/ArchiveSystem/Folder_view_data/Form_show_doc.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Configuration;
+using System.Diagnostics;
+
+namespace ArchiveSystem.Folder_view_data
+{
+
+
+
+    public partial class Form_show_doc : MetroFramework.Forms.MetroForm
+    {
+        public Form_show_doc()
+        {
+            InitializeComponent();
+        }
+        public static implicit operator Form_show_doc(ScanDialog v)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        String fn_g;
+
+        void check_Extension_file()
+        {
+
+            var Extension_file = Path.GetExtension(fn_g);
+
+
+            if (Extension_file == ".docx")
+            { ImageList_add_viwe.Images.Add(fn_g, ImageList_Extension.Images[0]); }
+            else if (Extension_file == ".pptx") //بور بوينت"
+            { ImageList_add_viwe.Images.Add(fn_g, ImageList_Extension.Images[1]); }
+            else if (Extension_file == ".xlsx") //ملف اكسل
+            { ImageList_add_viwe.Images.Add(fn_g, ImageList_Extension.Images[2]); }
+            else if (Extension_file == ".accdb") //'ملف اكسس
+            { ImageList_add_viwe.Images.Add(fn_g, ImageList_Extension.Images[3]); }
+            else if (Extension_file == ".txt") //'ملف نصي
+            { ImageList_add_viwe.Images.Add(fn_g, ImageList_Extension.Images[4]); }
+            else if (Extension_file == ".pdf") //'ملف pdf
+            { ImageList_add_viwe.Images.Add(fn_g, ImageList_Extension.Images[5]); }
+            else if (Extension_file == ".mp4") //'فيديو
+            { ImageList_add_viwe.Images.Add(fn_g, ImageList_Extension.Images[6]); }
+            else if (Extension_file == ".bnp" || Extension_file == ".bmp" || Extension_file == ".gif" || Extension_file == ".tif" || Extension_file == ".exe" || Extension_file == ".dll" || Extension_file == ".ico" || Extension_file == ".glp" || Extension_file == ".psd" || Extension_file == "." || Extension_file == ".xml" || Extension_file == ".html" || Extension_file == ".js" || Extension_file == ".css") //اخرى
+            { ImageList_add_viwe.Images.Add(fn_g, ImageList_Extension.Images[7]); }
+            else
+            { ImageList_add_viwe.Images.Add(fn_g, Bitmap.FromFile(fn_g)); }
+
+        }
+
+
+
+        private void Form_show_doc_Load(object sender, EventArgs e)
+        {
+            ListView_show_doc.Columns.Add("الملف", 300);
+            ListView_show_doc.View = View.LargeIcon;
+
+            ListView_show_doc.Items.Clear();
+            ImageList_add_viwe.Images.Clear();
+
+            ImageList_add_viwe.ImageSize = new Size(100, 100);
+            ListView_show_doc.Columns[0].Width = 120;
+
+            string path_folder_client_temp = ConfigurationManager.AppSettings["Path_Folder_Client_Temp"];
+
+            foreach (string file in System.IO.Directory.GetFiles(path_folder_client_temp))
+            {
+
+                fn_g = file;
+
+                check_Extension_file();
+
+
+                FileInfo fi = new FileInfo(fn_g);
+
+                //var files_n = new List<String>();
+                //files_n.Add(fi.FullName);
+
+                ListView_show_doc.Items.Add(fi.Name, ImageList_add_viwe.Images.Count - 1);
+
+            }
+          
+
+        }
+
+        
+    }
+}
--- ArchiveSystem/ArchiveSystem/Folder_view_data/Form_view_data_dqv.cs
+++ ArchiveSystem/ArchiveSystem/Folder_view_data/Form_view_data_dqv.cs
@@ -9,7 +9,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.IO;
+using System.Net;
+using System.Collections;
 
 namespace ArchiveSystem.Folder_view_data
 {
@@ -25,11 +27,11 @@ namespace ArchiveSystem.Folder_view_data
 
 
         DataTable dt = new DataTable();
-            SqlDataAdapter adapter;
+        SqlDataAdapter adapter;
 
         void fill_dgv_view_data_doc()
         {
-           
+
 
 
             adapter = new SqlDataAdapter(@"SELECT
@@ -69,7 +71,7 @@ FROM   dbo.ArchiveBooks_TBL INNER JOIN
             adapter.Fill(dt);
             advanc_dgv_view_data_doc.DataSource = dt;
             Label2_count_doc.Text = Convert.ToString(BindingContext[dt].Count);
-            Label2_count_doc_search.Text = Convert.ToString(BindingContext[dt].Count);
+
         }
 
 
@@ -86,9 +88,14 @@ FROM   dbo.ArchiveBooks_TBL INNER JOIN
 
 
             advanc_dgv_view_data_doc.Columns[0].HeaderCell.Style.BackColor = Color.DeepSkyBlue;
-           
             advanc_dgv_view_data_doc.ColumnHeadersDefaultCellStyle.Font = new Font("Tahoma", 11);
 
+
+            advanc_dgv_view_data_doc.AlternatingRowsDefaultCellStyle.BackColor = Color.Silver;
+            advanc_dgv_view_data_doc.RowsDefaultCellStyle.BackColor = Color.LightGray;
+
+            advanc_dgv_view_data_doc.RowsDefaultCellStyle.SelectionBackColor = Color.Orange;
+            advanc_dgv_view_data_doc.RowsDefaultCellStyle.SelectionForeColor = Color.Black;
         }
 
         private void btn_search_claer_Click(object sender, EventArgs e)
@@ -99,30 +106,30 @@ FROM   dbo.ArchiveBooks_TBL INNER JOIN
         private void txt_seach_TextChanged(object sender, EventArgs e)
         {
             try
-                {
-                  DataView dv = dt.DefaultView;
+            {
+                DataView dv = dt.DefaultView;
 
-                 dv.RowFilter = "[" + advanc_dgv_view_data_doc.Columns[col_index_select].Name + "]+[كود الكتاب]  Like '%" + txt_seach.Text + "%'";
-                  this.advanc_dgv_view_data_doc.DataSource = dv;
-                Label2_count_doc_search.Text = Convert.ToString(BindingContext[dt].Count);
+                dv.RowFilter = "[" + advanc_dgv_view_data_doc.Columns[col_index_select].Name + "]+[كود الكتاب]  Like '%" + txt_seach.Text + "%'";
+                this.advanc_dgv_view_data_doc.DataSource = dv;
+
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex + "يجب اختيار عمود لبحث بة");
-           }
+            }
 
-       
+
         }
 
         int col_index_select = 1;
         private void advanc_dgv_view_data_doc_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
         {
- txt_seach.Clear();
+            txt_seach.Clear();
 
             col_index_select = e.ColumnIndex;
 
-            for(int i = 0; i < advanc_dgv_view_data_doc.Columns.Count - 1; i++)
-               {
+            for (int i = 0; i < advanc_dgv_view_data_doc.Columns.Count - 1; i++)
+            {
 
                 advanc_dgv_view_data_doc.Columns[i].HeaderCell.Style.BackColor = Color.LightGray;
 
@@ -131,20 +138,165 @@ FROM   dbo.ArchiveBooks_TBL INNER JOIN
             }
             txt_seach.Select();
         }
-       
+
 
         private void NumericUpDown_font_size_ValueChanged(object sender, EventArgs e)
         {
 
             this.advanc_dgv_view_data_doc.ColumnHeadersDefaultCellStyle.Font = new Font("Tahoma", Convert.ToInt32(NumericUpDown_font_size.Value) + 1);
-            this.advanc_dgv_view_data_doc.DefaultCellStyle.Font = new Font("Tahoma", Convert.ToInt32( NumericUpDown_font_size.Value));
-            
+            this.advanc_dgv_view_data_doc.DefaultCellStyle.Font = new Font("Tahoma", Convert.ToInt32(NumericUpDown_font_size.Value));
+
         }
+
         private void advanc_dgv_view_data_doc_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
         {
             Label2_count_doc_search.Text = Convert.ToString(advanc_dgv_view_data_doc.RowCount);
         }
-     
 
+
+        /////////Code to read and download FTP files from the server/////////////
+        //You must first create a file on the server and set the FTP for it.To save files with
+
+        //1-Read FTP File
+
+        //This variables is Existing in a file App.config in the program
+        string ftp_server_Ip = ConfigurationManager.AppSettings["FTP_Server_Ip"];
+        string ftp_server_username = ConfigurationManager.AppSettings["FTP_Server_user"];
+        string ftp_server_password = ConfigurationManager.AppSettings["FTP_Server_pass"];
+
+        //The path of the file on the client computer with which we will download the files from the FTP file temporarily, and then we delete the downloaded files
+        string path_folder_client_temp = ConfigurationManager.AppSettings["Path_Folder_Client_Temp"];
+
+        public string[] GetFileList()
+        {
+
+
+            string[] downloadFiles;
+            StringBuilder result = new StringBuilder();
+            FtpWebRequest reqFTP;
+            try
+            {
+                //                                          Here we put the path IP and of the FTP file server
+                //reqFTP = (FtpWebRequest)FtpWebRequest.Create(new Uri(ftp_server_Ip + @"wared\cjs2\"));
+                reqFTP = (FtpWebRequest)FtpWebRequest.Create(new Uri(ftp_server_Ip + @"\" + advanc_dgv_view_data_doc.CurrentRow.Cells[6].Value.ToString() + @"\" + advanc_dgv_view_data_doc.CurrentRow.Cells[0].Value.ToString() + @"\"));
+                reqFTP.UseBinary = true;
+                reqFTP.Credentials = new NetworkCredential(ftp_server_username, ftp_server_password);
+                reqFTP.Method = WebRequestMethods.Ftp.ListDirectory;
+                WebResponse response = reqFTP.GetResponse();
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    result.Append(line);
+                    result.Append("\n");
+                    line = reader.ReadLine();
+                }
+                // to remove the trailing '\n'
+                result.Remove(result.ToString().LastIndexOf('\n'), 1);
+                reader.Close();
+                response.Close();
+
+                return result.ToString().Split('\n');
+
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+                downloadFiles = null;
+                return downloadFiles;
+            }
+        }
+
+        //2-Download FTP Files
+        private void Download(string fileName)
+        {
+
+            FtpWebRequest reqFTP;
+            try
+            {
+
+                //filePath = <<The full path where the file is to be created. the>>,
+                //fileName = <<Name of the file to be createdNeed not name on FTP server. name name()>>
+                FileStream outputStream = new FileStream(path_folder_client_temp + "\\" + fileName, FileMode.Create);
+                //                                           Here we put the path IP, and file name of the FTP file server
+                //reqFTP = (FtpWebRequest)FtpWebRequest.Create(new Uri(ftp_server_Ip + @"wared\cjs2\" + fileName)); 
+                reqFTP = (FtpWebRequest)FtpWebRequest.Create(new Uri(ftp_server_Ip + @"\" + advanc_dgv_view_data_doc.CurrentRow.Cells[6].Value.ToString() + @"\" + advanc_dgv_view_data_doc.CurrentRow.Cells[0].Value.ToString() + @"\" + fileName));
+                reqFTP.Method = WebRequestMethods.Ftp.DownloadFile;
+                reqFTP.UseBinary = true;
+                reqFTP.Credentials = new NetworkCredential(ftp_server_username, ftp_server_password);
+                FtpWebResponse response = (FtpWebResponse)reqFTP.GetResponse();
+                Stream ftpStream = response.GetResponseStream();
+                long cl = response.ContentLength;
+                int bufferSize = 2048;
+                int readCount;
+                byte[] buffer = new byte[bufferSize];
+                readCount = ftpStream.Read(buffer, 0, bufferSize);
+                while (readCount > 0)
+                {
+                    outputStream.Write(buffer, 0, readCount);
+                    readCount = ftpStream.Read(buffer, 0, bufferSize);
+                }
+                ftpStream.Close();
+                outputStream.Close();
+                response.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void advanc_dgv_view_data_doc_CellDoubleClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+           try
+            {
+             
+                string[] files = GetFileList();
+
+                System.IO.DirectoryInfo di = new DirectoryInfo(path_folder_client_temp);
+
+
+
+
+
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    ////////////important code/////////////
+                    //It allows us to delete when the file is used by the processor
+                    System.GC.Collect();
+                    System.GC.WaitForPendingFinalizers();
+                    //----------end-------------
+
+                    file.Delete();
+
+                }
+
+
+
+                foreach (string file in files)
+                {
+
+                    Download(file);
+                }
+
+
+                var path = string.Format(path_folder_client_temp);
+
+                Form_show_doc s_doc1 = new Form_show_doc();
+                s_doc1.Show();
+
+                //System.Diagnostics.Process.Start(path);
+
+            }
+            catch (WebException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+      
+
+        //-----------------END------------------------
     }
 }
