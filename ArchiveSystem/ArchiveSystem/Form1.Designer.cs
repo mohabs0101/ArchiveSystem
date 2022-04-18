ff424d19bdb5979ed553cc2eb164a7bfd2b457ca
mohammed01it <91244043+mohammed01it@users.noreply.github.com>
2022-04-18 09:55:11 +0300

    18

--- ArchiveSystem/ArchiveSystem/Form1.Designer.cs
+++ ArchiveSystem/ArchiveSystem/Form1.Designer.cs
@@ -57,11 +57,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.Scanning_Folder = new System.Windows.Forms.Button();
             this.panel12 = new System.Windows.Forms.Panel();
-            this.COM_bookStatus = new System.Windows.Forms.ComboBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.COM_priority = new System.Windows.Forms.ComboBox();
             this.COM_privicy = new System.Windows.Forms.ComboBox();
-            this.COM_PaperType = new System.Windows.Forms.ComboBox();
             this.COM_bookType = new System.Windows.Forms.ComboBox();
             this.DT_bookRecive_date = new System.Windows.Forms.DateTimePicker();
             this.DT_bookDate = new System.Windows.Forms.DateTimePicker();
@@ -123,6 +120,7 @@
             this.lbl_loginDepartment = new System.Windows.Forms.Label();
             this.lbl_LoginUsername = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.COM_PaperType = new System.Windows.Forms.ComboBox();
             this.panel7.SuspendLayout();
             this.metroTabControl1.SuspendLayout();
             this.metroTabPage1.SuspendLayout();
@@ -206,7 +204,7 @@
             this.metroTabControl1.Location = new System.Drawing.Point(10, 42);
             this.metroTabControl1.Name = "metroTabControl1";
             this.metroTabControl1.RightToLeftLayout = true;
-            this.metroTabControl1.SelectedIndex = 0;
+            this.metroTabControl1.SelectedIndex = 1;
             this.metroTabControl1.Size = new System.Drawing.Size(1108, 390);
             this.metroTabControl1.TabIndex = 9;
             this.metroTabControl1.UseSelectable = true;
@@ -480,8 +478,6 @@
             // 
             this.panel12.AutoScroll = true;
             this.panel12.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.panel12.Controls.Add(this.COM_bookStatus);
-            this.panel12.Controls.Add(this.label2);
             this.panel12.Controls.Add(this.COM_priority);
             this.panel12.Controls.Add(this.COM_privicy);
             this.panel12.Controls.Add(this.COM_PaperType);
@@ -515,29 +511,6 @@
             this.panel12.Size = new System.Drawing.Size(723, 328);
             this.panel12.TabIndex = 3;
             // 
-            // COM_bookStatus
-            // 
-            this.COM_bookStatus.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.COM_bookStatus.FormattingEnabled = true;
-            this.COM_bookStatus.Items.AddRange(new object[] {
-            "منتهي",
-            "قيد المتابعة"});
-            this.COM_bookStatus.Location = new System.Drawing.Point(198, 201);
-            this.COM_bookStatus.Name = "COM_bookStatus";
-            this.COM_bookStatus.Size = new System.Drawing.Size(136, 21);
-            this.COM_bookStatus.TabIndex = 12;
-            // 
-            // label2
-            // 
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Calibri", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(340, 177);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(77, 21);
-            this.label2.TabIndex = 167;
-            this.label2.Text = "حالة الكتاب";
-            // 
             // COM_priority
             // 
             this.COM_priority.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
@@ -561,18 +534,6 @@
             this.COM_privicy.Name = "COM_privicy";
             this.COM_privicy.Size = new System.Drawing.Size(136, 21);
             this.COM_privicy.TabIndex = 9;
-            // 
-            // COM_PaperType
-            // 
-            this.COM_PaperType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.COM_PaperType.FormattingEnabled = true;
-            this.COM_PaperType.Items.AddRange(new object[] {
-            "استنساخ",
-            "اصلي"});
-            this.COM_PaperType.Location = new System.Drawing.Point(384, 202);
-            this.COM_PaperType.Name = "COM_PaperType";
-            this.COM_PaperType.Size = new System.Drawing.Size(136, 21);
-            this.COM_PaperType.TabIndex = 11;
             // 
             // COM_bookType
             // 
@@ -605,7 +566,7 @@
             this.BTN_Archive.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.BTN_Archive.BackColor = System.Drawing.Color.MediumAquamarine;
             this.BTN_Archive.Font = new System.Drawing.Font("Marlett", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BTN_Archive.Location = new System.Drawing.Point(692, 360);
+            this.BTN_Archive.Location = new System.Drawing.Point(702, 360);
             this.BTN_Archive.Name = "BTN_Archive";
             this.BTN_Archive.Size = new System.Drawing.Size(142, 41);
             this.BTN_Archive.TabIndex = 16;
@@ -635,7 +596,7 @@
             this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Calibri", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(718, 9);
+            this.label7.Location = new System.Drawing.Point(728, 9);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(72, 21);
             this.label7.TabIndex = 136;
@@ -646,7 +607,7 @@
             this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Calibri", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(707, 66);
+            this.label8.Location = new System.Drawing.Point(717, 66);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(83, 21);
             this.label8.TabIndex = 137;
@@ -657,7 +618,7 @@
             this.label16.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label16.AutoSize = true;
             this.label16.Font = new System.Drawing.Font("Calibri", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label16.Location = new System.Drawing.Point(552, 120);
+            this.label16.Location = new System.Drawing.Point(562, 120);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(50, 21);
             this.label16.TabIndex = 145;
@@ -668,7 +629,7 @@
             this.label9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Calibri", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(728, 120);
+            this.label9.Location = new System.Drawing.Point(738, 120);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(63, 21);
             this.label9.TabIndex = 138;
@@ -687,7 +648,7 @@
             this.label10.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Calibri", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(717, 176);
+            this.label10.Location = new System.Drawing.Point(727, 176);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(74, 21);
             this.label10.TabIndex = 139;
@@ -698,7 +659,7 @@
             this.label11.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Calibri", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(357, 9);
+            this.label11.Location = new System.Drawing.Point(367, 9);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(60, 21);
             this.label11.TabIndex = 140;
@@ -709,7 +670,7 @@
             this.label21.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label21.AutoSize = true;
             this.label21.Font = new System.Drawing.Font("Calibri", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label21.Location = new System.Drawing.Point(530, 66);
+            this.label21.Location = new System.Drawing.Point(540, 66);
             this.label21.Name = "label21";
             this.label21.Size = new System.Drawing.Size(73, 21);
             this.label21.TabIndex = 150;
@@ -720,7 +681,7 @@
             this.label12.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label12.AutoSize = true;
             this.label12.Font = new System.Drawing.Font("Calibri", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label12.Location = new System.Drawing.Point(531, 9);
+            this.label12.Location = new System.Drawing.Point(541, 9);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(71, 21);
             this.label12.TabIndex = 141;
@@ -731,7 +692,7 @@
             this.label13.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label13.AutoSize = true;
             this.label13.Font = new System.Drawing.Font("Calibri", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label13.Location = new System.Drawing.Point(389, 66);
+            this.label13.Location = new System.Drawing.Point(399, 66);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(27, 21);
             this.label13.TabIndex = 142;
@@ -751,7 +712,7 @@
             this.label14.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label14.AutoSize = true;
             this.label14.Font = new System.Drawing.Font("Calibri", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label14.Location = new System.Drawing.Point(393, 120);
+            this.label14.Location = new System.Drawing.Point(403, 120);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(24, 21);
             this.label14.TabIndex = 143;
@@ -790,7 +751,7 @@
             this.label17.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label17.AutoSize = true;
             this.label17.Font = new System.Drawing.Font("Calibri", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label17.Location = new System.Drawing.Point(528, 176);
+            this.label17.Location = new System.Drawing.Point(538, 176);
             this.label17.Name = "label17";
             this.label17.Size = new System.Drawing.Size(74, 21);
             this.label17.TabIndex = 146;
@@ -879,7 +840,6 @@
             // 
             this.DGV_assignation.AllowUserToAddRows = false;
             this.DGV_assignation.AllowUserToDeleteRows = false;
-            this.DGV_assignation.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.DGV_assignation.Dock = System.Windows.Forms.DockStyle.Fill;
             this.DGV_assignation.FilterAndSortEnabled = true;
             this.DGV_assignation.FilterStringChangedInvokeBeforeDatasourceUpdate = true;
@@ -1213,6 +1173,18 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // COM_PaperType
+            // 
+            this.COM_PaperType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.COM_PaperType.FormattingEnabled = true;
+            this.COM_PaperType.Items.AddRange(new object[] {
+            "استنساخ",
+            "اصلي"});
+            this.COM_PaperType.Location = new System.Drawing.Point(389, 202);
+            this.COM_PaperType.Name = "COM_PaperType";
+            this.COM_PaperType.Size = new System.Drawing.Size(136, 21);
+            this.COM_PaperType.TabIndex = 11;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -1347,14 +1319,11 @@
         private System.Windows.Forms.Button btn_view_data_dqv;
         private System.Windows.Forms.ComboBox COM_priority;
         private System.Windows.Forms.ComboBox COM_privicy;
-        private System.Windows.Forms.ComboBox COM_PaperType;
         private System.Windows.Forms.ComboBox COM_bookType;
         private System.Windows.Forms.DateTimePicker DT_bookRecive_date;
         private System.Windows.Forms.DateTimePicker DT_bookDate;
         private System.Windows.Forms.Label LBL_department;
         private System.Windows.Forms.Label LBL_USERNAME;
-        private System.Windows.Forms.ComboBox COM_bookStatus;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button FOLDERS_prefermCLick;
         private System.Windows.Forms.Button BTN_RefrshFolders;
         private System.Windows.Forms.Panel panel32;
@@ -1371,6 +1340,7 @@
         private System.Windows.Forms.Label LBL_FTPconValue;
         private System.Windows.Forms.PictureBox PICBOX_failICON;
         private Zuby.ADGV.AdvancedDataGridView DGV_assignation;
+        private System.Windows.Forms.ComboBox COM_PaperType;
     }
 }
 
