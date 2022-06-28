c081c06195a71345d87fa332e4fb2c303a40a51b
mohammed01it <91244043+mohammed01it@users.noreply.github.com>
2022-06-28 11:44:25 +0300

    com 11

--- ArchiveSystem/ArchiveSystem/Login.Designer.cs
+++ ArchiveSystem/ArchiveSystem/Login.Designer.cs
@@ -34,13 +34,13 @@
             this.panel4 = new System.Windows.Forms.Panel();
             this.panel5 = new System.Windows.Forms.Panel();
             this.panel8 = new System.Windows.Forms.Panel();
-            this.TXT_User = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.TXT_User = new System.Windows.Forms.TextBox();
             this.TXT_pass = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.BTN_Login = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel6 = new System.Windows.Forms.Panel();
             this.panel7 = new System.Windows.Forms.Panel();
             this.panel5.SuspendLayout();
@@ -52,36 +52,40 @@
             // 
             this.panel1.BackColor = System.Drawing.Color.White;
             this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel1.Location = new System.Drawing.Point(715, 60);
+            this.panel1.Location = new System.Drawing.Point(834, 74);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(10, 448);
+            this.panel1.Size = new System.Drawing.Size(12, 551);
             this.panel1.TabIndex = 5;
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.White;
             this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel2.Location = new System.Drawing.Point(20, 60);
+            this.panel2.Location = new System.Drawing.Point(23, 74);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(10, 448);
+            this.panel2.Size = new System.Drawing.Size(12, 551);
             this.panel2.TabIndex = 6;
             // 
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.White;
             this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel3.Location = new System.Drawing.Point(30, 60);
+            this.panel3.Location = new System.Drawing.Point(35, 74);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(685, 10);
+            this.panel3.Size = new System.Drawing.Size(799, 12);
             this.panel3.TabIndex = 7;
             // 
             // panel4
             // 
             this.panel4.BackColor = System.Drawing.Color.White;
             this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel4.Location = new System.Drawing.Point(30, 498);
+            this.panel4.Location = new System.Drawing.Point(35, 613);
+            this.panel4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(685, 10);
+            this.panel4.Size = new System.Drawing.Size(799, 12);
             this.panel4.TabIndex = 8;
             // 
             // panel5
@@ -92,9 +96,10 @@
             this.panel5.Controls.Add(this.panel6);
             this.panel5.Controls.Add(this.panel7);
             this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel5.Location = new System.Drawing.Point(30, 70);
+            this.panel5.Location = new System.Drawing.Point(35, 86);
+            this.panel5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(685, 428);
+            this.panel5.Size = new System.Drawing.Size(799, 527);
             this.panel5.TabIndex = 9;
             // 
             // panel8
@@ -108,22 +113,11 @@
             this.panel8.Controls.Add(this.label1);
             this.panel8.Controls.Add(this.BTN_Login);
             this.panel8.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel8.Location = new System.Drawing.Point(0, 100);
+            this.panel8.Location = new System.Drawing.Point(0, 123);
+            this.panel8.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel8.Name = "panel8";
-            this.panel8.Size = new System.Drawing.Size(685, 241);
+            this.panel8.Size = new System.Drawing.Size(799, 297);
             this.panel8.TabIndex = 20;
-            // 
-            // TXT_User
-            // 
-            this.TXT_User.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.TXT_User.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.TXT_User.Font = new System.Drawing.Font("Calibri", 12.75F);
-            this.TXT_User.Location = new System.Drawing.Point(368, 63);
-            this.TXT_User.Name = "TXT_User";
-            this.TXT_User.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.TXT_User.Size = new System.Drawing.Size(271, 28);
-            this.TXT_User.TabIndex = 11;
-            this.TXT_User.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label3
             // 
@@ -132,11 +126,24 @@
             this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.label3.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.label3.Location = new System.Drawing.Point(42, 3);
+            this.label3.Location = new System.Drawing.Point(49, 4);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(215, 23);
+            this.label3.Size = new System.Drawing.Size(272, 29);
             this.label3.TabIndex = 16;
             this.label3.Text = "ارشيف مديرية الهندسة العسكرية";
+            // 
+            // TXT_User
+            // 
+            this.TXT_User.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.TXT_User.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TXT_User.Font = new System.Drawing.Font("Calibri", 12.75F);
+            this.TXT_User.Location = new System.Drawing.Point(429, 78);
+            this.TXT_User.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TXT_User.Name = "TXT_User";
+            this.TXT_User.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.TXT_User.Size = new System.Drawing.Size(316, 33);
+            this.TXT_User.TabIndex = 11;
+            this.TXT_User.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // TXT_pass
             // 
@@ -144,24 +151,38 @@
             this.TXT_pass.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.TXT_pass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.TXT_pass.Font = new System.Drawing.Font("Calibri", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TXT_pass.Location = new System.Drawing.Point(368, 120);
+            this.TXT_pass.Location = new System.Drawing.Point(429, 148);
+            this.TXT_pass.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.TXT_pass.Name = "TXT_pass";
             this.TXT_pass.PasswordChar = '*';
             this.TXT_pass.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.TXT_pass.Size = new System.Drawing.Size(271, 28);
+            this.TXT_pass.Size = new System.Drawing.Size(316, 33);
             this.TXT_pass.TabIndex = 14;
             this.TXT_pass.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.TXT_pass.UseSystemPasswordChar = true;
             this.TXT_pass.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TXT_pass_KeyDown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = global::ArchiveSystem.Properties.Resources.zip_icon;
+            this.pictureBox1.Location = new System.Drawing.Point(7, 34);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(334, 199);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
             // 
             // label2
             // 
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Calibri", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(566, 96);
+            this.label2.Location = new System.Drawing.Point(660, 118);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 21);
+            this.label2.Size = new System.Drawing.Size(96, 27);
             this.label2.TabIndex = 13;
             this.label2.Text = "كلمة المرور";
             // 
@@ -170,9 +191,9 @@
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Calibri", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(545, 39);
+            this.label1.Location = new System.Drawing.Point(636, 48);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(94, 21);
+            this.label1.Size = new System.Drawing.Size(120, 27);
             this.label1.TabIndex = 12;
             this.label1.Text = "اسم المستخدم";
             // 
@@ -181,35 +202,25 @@
             this.BTN_Login.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.BTN_Login.BackColor = System.Drawing.Color.White;
             this.BTN_Login.Font = new System.Drawing.Font("Calibri", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BTN_Login.Location = new System.Drawing.Point(524, 177);
+            this.BTN_Login.Location = new System.Drawing.Point(611, 218);
+            this.BTN_Login.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.BTN_Login.Name = "BTN_Login";
-            this.BTN_Login.Size = new System.Drawing.Size(115, 38);
+            this.BTN_Login.Size = new System.Drawing.Size(134, 47);
             this.BTN_Login.TabIndex = 17;
             this.BTN_Login.Text = "تسجيل الدخول";
             this.BTN_Login.UseVisualStyleBackColor = false;
             this.BTN_Login.Click += new System.EventHandler(this.BTN_Login_Click);
             this.BTN_Login.Enter += new System.EventHandler(this.BTN_Login_Click);
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.Image = global::ArchiveSystem.Properties.Resources.zip_icon;
-            this.pictureBox1.Location = new System.Drawing.Point(6, 28);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(286, 162);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 15;
-            this.pictureBox1.TabStop = false;
-            // 
             // panel6
             // 
             this.panel6.BackgroundImage = global::ArchiveSystem.Properties.Resources.footer2;
             this.panel6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.panel6.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel6.Location = new System.Drawing.Point(0, 341);
+            this.panel6.Location = new System.Drawing.Point(0, 420);
+            this.panel6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(685, 87);
+            this.panel6.Size = new System.Drawing.Size(799, 107);
             this.panel6.TabIndex = 19;
             // 
             // panel7
@@ -218,23 +229,26 @@
             this.panel7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.panel7.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel7.Location = new System.Drawing.Point(0, 0);
+            this.panel7.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(685, 100);
+            this.panel7.Size = new System.Drawing.Size(799, 123);
             this.panel7.TabIndex = 18;
             // 
             // Login
             // 
             this.AcceptButton = this.BTN_Login;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(745, 528);
+            this.ClientSize = new System.Drawing.Size(869, 650);
             this.Controls.Add(this.panel5);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Login";
+            this.Padding = new System.Windows.Forms.Padding(23, 74, 23, 25);
             this.Style = MetroFramework.MetroColorStyle.Teal;
             this.Text = "تسجيل الدخول";
             this.TextAlign = MetroFramework.Forms.MetroFormTextAlign.Right;
